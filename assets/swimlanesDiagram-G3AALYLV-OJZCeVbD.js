9d0b24f2174488571726f8bd2d899d5f2f111681
Chaitanya V <cnjv1999@gmail.com>
2026-07-13 01:41:01 +0530

    Updates

---
+++ assets/swimlanesDiagram-G3AALYLV-OJZCeVbD.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-23GEKE2U-RccYZHB7.js";import{_ as a}from"./index-Bt-AhzM0.js";import"./chunk-5VM5RSS4-B3b1PV1V.js";import"./chunk-XXDRQBXY-BsNk8feE.js";import"./chunk-VR4S4FIN-uCeSZEH-.js";import"./chunk-32BRIVSS-DpRGuCIl.js";import"./channel-BRx00pb7.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
