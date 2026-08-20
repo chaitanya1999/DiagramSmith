1db2503bf10357575f93e2e31cde8e9d37d213bf
Chaitanya V <cnjv1999@gmail.com>
2026-08-20 10:08:36 +0530

    Updates

--- assets/swimlanesDiagram-G3AALYLV-r12mkbMM.js
+++
@@ -1,8 +0,0 @@
-import{c as e,s as r}from"./flowDiagram-23GEKE2U-C1DWpqhV.js";import{_ as a}from"./index-CYSMz3fm.js";import"./chunk-5VM5RSS4-DVXMp_aV.js";import"./chunk-XXDRQBXY-BtQuI5xs.js";import"./chunk-VR4S4FIN-DyYPLCKe.js";import"./chunk-32BRIVSS-B7BRNje_.js";import"./channel-Dy240XcU.js";var o=a(t=>`${r(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
